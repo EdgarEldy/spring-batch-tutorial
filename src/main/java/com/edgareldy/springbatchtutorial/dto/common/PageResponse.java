950fe370fee37d77211e9aec9bb99a30406f14dc
EdgarEldy <ed.eldy21@live.com>
2026-08-17 13:52:55 +0400

    docs(core): add mandatory class javadoc to PageResponse
    - Adds the class-level javadoc block (description, Created by/Author/Date/Project) required by the project's mandatory class-level Javadoc convention.

--- src/main/java/com/edgareldy/springbatchtutorial/dto/common/PageResponse.java
+++ src/main/java/com/edgareldy/springbatchtutorial/dto/common/PageResponse.java
@@ -6,6 +6,11 @@ import org.springframework.data.domain.Page;
 /**
  * Generic paginated content DTO used as the {@code data} payload of
  * {@link ApiResponse} on every list endpoint, instead of a plain list.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/15/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/15/26
+ * Project : spring-batch-tutorial
  */
 public record PageResponse<T>(
         List<T> content,
