2af9ae698b260c0cff7027091a2c29ae5249c617
EdgarEldy <ed.eldy21@live.com>
2026-08-17 13:52:55 +0400

    docs(core): add mandatory class javadoc to ResourceNotFoundException
    Retroactive Javadoc footer for ResourceNotFoundException, the last of the pre-convention classes in this branch still missing the required block.

--- src/main/java/com/edgareldy/springbatchtutorial/exception/ResourceNotFoundException.java
+++ src/main/java/com/edgareldy/springbatchtutorial/exception/ResourceNotFoundException.java
@@ -4,6 +4,11 @@ package com.edgareldy.springbatchtutorial.exception;
  * Thrown when a requested entity does not exist (e.g. looking up a
  * PayrollRun by an id that is not in the database). Caught by
  * {@link GlobalExceptionHandler} and translated into a 404 response.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/15/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/15/26
+ * Project : spring-batch-tutorial
  */
 public class ResourceNotFoundException extends RuntimeException {
 
